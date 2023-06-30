cadd8151e7a3732048c64717e38610f082a80adb
ondini <kafka.ondrej@email.cz>
2023-06-30 15:55:41 +0200

    Trace filtering added.

--- lgcgenapp/src/sections/Histogram.jsx
+++ lgcgenapp/src/sections/Histogram.jsx
@@ -5,23 +5,14 @@ import { getResiduals } from "../utils/dataProcessing";
 import Title from "../components/Title";
 import "./Histogram.css";
 
-const makeBins = (data, key, nbinsx) => {
-  const maxVal = Math.max(...data[key]);
-  const minVal = Math.min(...data[key]);
-  const binSize = (maxVal - minVal) / nbinsx;
-  let xbins = {
-    end: maxVal,
-    size: binSize,
-    start: minVal,
-  };
-
+const makeBinDescs = (data, key, nbinsx, binsx) => {
   let binsDescs = Array(nbinsx).fill("");
   let binsCounts = Array(nbinsx).fill(0);
   const maxBinDescCount = 20;
 
   for (let i = 0; i < data[key].length; i++) {
     const value = data[key][i];
-    const binIndex = Math.floor((value - minVal) / binSize);
+    const binIndex = Math.floor((value - binsx.start) / binsx.size);
     binsCounts[binIndex] += 1;
     binsDescs[binIndex] =
       binsDescs[binIndex] +
@@ -39,13 +30,18 @@ const makeBins = (data, key, nbinsx) => {
         : "");
   }
 
-  return { xbins: xbins, customdata: binsDescs };
+  let i = 0;
+  while (binsCounts[i] == 0) {
+    i++;
+  }
+
+  return { xbins: binsx, customdata: binsDescs.slice(i) };
 };
 
 const makeTraces = (residuals, key, nbinsx) => {
   let traces = {};
   let traceTemp = {};
-  residuals.forEach((key) => {
+  Object.keys(residuals).forEach((key) => {
     traceTemp[key] = [];
   });
   for (let i = 0; i < residuals[key].length; i++) {
@@ -53,36 +49,55 @@ const makeTraces = (residuals, key, nbinsx) => {
     if (traces[traceKey] === undefined) {
       traces[traceKey] = JSON.parse(JSON.stringify(traceTemp));
     }
-    residuals.forEach((key2) => {
+    Object.keys(residuals).forEach((key2) => {
       traces[traceKey][key2].push(residuals[key2][i]);
     });
   }
   return traces;
 };
 
-const makePlotData = (residuals, key, nbinsx) => {
-  let resBinData = makeBins(residuals, key, nbinsx);
+const makePlotLayout = (residuals, key, nbinsx, binsx, name) => {
+  let resBinData = makeBinDescs(residuals, key, nbinsx, binsx);
 
-  return [
-    {
-      x: residuals[key],
-      customdata: resBinData.customdata, // customdata is used to display the obs. information in the hovertemplate
-      type: "histogram",
-      autobinx: false,
-      xbins: resBinData.xbins,
-
-      marker: {
-        color: "rgba(100, 200, 102, 0.7)",
-        line: {
-          color: "rgba(100, 200, 102, 1)",
-          width: 1,
-        },
+  return {
+    x: residuals[key],
+    customdata: resBinData.customdata, // customdata is used to display the obs. information in the hovertemplate
+    type: "histogram",
+    autobinx: false,
+    xbins: resBinData.xbins,
+    name: name,
+    marker: {
+      // color: "rgba(100, 200, 102, 0.7)",
+      line: {
+        // color: "rgba(100, 200, 102, 1)",
+        width: 1,
       },
-      opacity: 0.75,
-
-      hovertemplate: "<-b> Count: %{y} </b>" + "%{customdata} <extra></extra>",
     },
-  ];
+    opacity: 0.75,
+
+    hovertemplate: "<b> Count: %{y} </b>" + "%{customdata} <extra></extra>",
+  };
+};
+
+const makePlotData = (residuals, key, nbinsx) => {
+  const maxVal = Math.max(...residuals[key]);
+  const minVal = Math.min(...residuals[key]);
+  const binSize = (maxVal - minVal) / nbinsx;
+  let xbins = {
+    end: maxVal,
+    size: binSize,
+    start: minVal,
+  };
+
+  let mkTrcs = true;
+  const traces = mkTrcs
+    ? makeTraces(residuals, key, nbinsx)
+    : { ";": residuals };
+  let plotData = [];
+  Object.keys(traces).forEach((key2) => {
+    plotData.push(makePlotLayout(traces[key2], key, nbinsx, xbins, key2));
+  });
+  return plotData;
 };
 
 const Histogram = ({ data }) => {
@@ -95,11 +110,12 @@ const Histogram = ({ data }) => {
     Object.keys(residuals[measType]).forEach((key) => {
       if (nonResKeys.includes(key)) return;
 
+      console.log(makePlotData(residuals[measType], key, 30));
       histograms.push(
         <div className="histsec-plots-plot">
           <Plot
             data={makePlotData(residuals[measType], key, 30)}
-            layout={{ title: key, bargroupgap: 0.2 }}
+            layout={{ title: key, bargroupgap: 0.2, barmode: "stack" }}
           />
         </div>
       );
