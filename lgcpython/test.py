808264cf3d0eeca9caa2a41f1e9a788d22eb48b7
ondini <kafka.ondrej@email.cz>
2023-08-09 17:26:48 +0200

    Restruct.

--- lgcpython/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-json_path = 'C:\\Users\\okafka\\Developer\\LGCReportGenerator\\lgcgenapp\\src\\data\\Example.json'
-
-if __name__ == "__main__":
-    with open(json_path, 'r') as f:
-        data = json.load(f)
-        print(data)
